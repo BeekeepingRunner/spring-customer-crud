b3fd9f2b2b728083d7d5d95377c480a4536b7f56
Bartosz Biegajło <biegbart.z@gmail.com>
2021-06-01 20:32:36 +0200

    add proper @Pointcut declarations

--- src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,4 +11,18 @@ import org.springframework.stereotype.Component;
 public class CRMLoggingAspect {
 
 	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	@Pointcut("execute(* com.luv2code.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execute(* com.luv2code.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execute(* com.luv2code.springdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	
 }
